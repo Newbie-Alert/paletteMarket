e71337ac9958216a051272d1ce35f5dd00bef681
Liabinn <liabin124@gmail.com>
2024-01-16 22:40:58 +0900

    [UPDATE] type add

--- src/components/prducts/ProductsType.ts
+++ src/components/prducts/ProductsType.ts
@@ -12,3 +12,31 @@ export type TextRadioValueType = {
   address: string,
   detailAddress: string
 }
+
+export type ProductsPostType = {
+  id:string,
+  post_user: string,
+  nickname: string,    
+  created_at: string,
+  title: string,
+  category: string[],
+  price: number,
+  shipping_cost: string,
+  count: number, 
+  deal_type: string,
+  address: string,
+  detailAddress: string,
+  quality: string,
+  changable: string,
+  exchange_product: string,
+  contents: string,
+  tags: string[], 
+  agreement: boolean,
+  like_user: {uid:string, }[],
+  likes: number,
+};
+
+export interface Props {
+  products: ProductsPostType[],
+  setProducts: React.Dispatch<React.SetStateAction<ProductsPostType>>
+}
